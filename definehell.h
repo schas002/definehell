827184902252f1b9d4a06863b9855805c9356505
schas002 <schas002@users.noreply.github.com>
2016-09-21 20:30:26 +0300

    Upload define hell

---
+++ definehell.h
@@ -0,0 +1,57 @@
+#pragma once
+
+// arithmetic hell
+#define plus +
+#define subtract -
+#define times *
+#define divide /
+#define modulo %
+#define doubled * 2
+#define tripled * 3
+#define quadrupled * 4
+#define quintupled * 5
+#define hextupled * 6
+#define heptupled * 7
+#define octupled * 8
+#define enntupled * 9
+#define dectupled * 10
+#define half / 2
+#define quarter / 4
+#define increment ++
+#define decrement --
+
+// conditional hell
+#define equals ==
+#define less <
+#define more >
+#define isnt !=
+#define atmost <=
+#define atleast >=
+#define truthy != 0
+#define falsy == 0
+
+// int main() hell
+#define program int main()
+#define yea return 0;
+#define nay return 1;
+
+// stdio.h hell
+#define print printf(
+#define input scanf(
+#define pointer &
+
+// symbol hell
+#define yes 1
+#define no 0
+#define begin {
+#define then {
+#define end }
+#define so );
+#define next ;
+#define also ,
+#define wax (
+#define wane )
+#define doing ) {
+
+// variable hell
+#define now =
